1979984c6ebc1efbb061fe4ebae7cd20d29dedd2
Jeff Harris <techjeffharris@gmail.com>
2015-02-05 23:31:02 -0800

    REALLY fixes #24 this time, I promise!

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-npm install -g jsdoc jsdoc-to-markdown
+npm install jsdoc jsdoc-to-markdown
 
-./node_modules/jsdoc -c jsdoc.conf.json -d ./docs/ -R ./README.md --verbose ./lib/
+jsdoc -c jsdoc.conf.json -d ./docs/ -R ./README.md --verbose ./lib/
 
 jsdoc2md lib/*.js > ./docs/FriendsOfFriends.md
